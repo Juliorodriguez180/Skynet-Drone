e056dcb01e1792f199eeef79d165589eb0088501
Skynet Drone <julrodri@uat.edu>
2018-01-30 13:17:26 -0700

    Start of Skynet Drone

--- Skynet Drone/Skynet Drone.cpp
+++ Skynet Drone/Skynet Drone.cpp
@@ -2,10 +2,31 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+//This is needed for random number seeding
+#include <ctime>
+using std:cout;
+using std::endl;
+
 
 
 int main()
 {
-    return 0;
+ 
+	//Generate Random Enemy Location on 8x8 grid.
+	cout << "Generate Random Enemy Location on 8x8 grid...." << endl;
+	// Required a really randomized nnu,ber by the help
+
+	srand(time(0));
+
+
+	int searchGridHighNumber = 64
+		int searchGridLowNumber = 1
+		int enemyLocation = rand() % searchGridHighNumber + searchGridLowNumber;
+
+	cout << "The Enemy is located at location"
+
+	   
+	return 0;
 }
 
